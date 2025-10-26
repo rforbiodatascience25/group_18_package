caf0a04d319880ab9567be5f1fb55a622f2a6dc1
arwynnnn <kacperthe@gmail.com>
2025-10-26 23:48:10 +0100

    add codon_table data object

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Codon Table
+#'
+#' This decodes nucleic acids into amino acid residues.
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
