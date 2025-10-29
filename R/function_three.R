b8db4e4bb39ede90332f05942bcf05f56144c6c9
loayzapre <s252608@dtu.dk>
2025-10-29 22:50:19 +0100

    Function 3 and Function 5 fixed, Description Imports updated and Vignette for pdf created

--- R/function_three.R
+++ R/function_three.R
@@ -2,17 +2,17 @@
 #'
 #' @param seq_DNA DNA sequence
 #' @param codon_split splits DNA into codons
-#' @param length length of the sequence
 #'
 #' @returns it returns codons from DNA sequence
 #' @export
 #'
-#' @examples split_codons("CUCCUACUG")
+#' @examples
+#' codon_split("CUCCUACUG")
 codon_split <- function(seq_DNA, start = 1){
-  length <- nchar(seq_DNA) #get the length of DNA sequence
+  seq_length <- nchar(seq_DNA) #get the length of DNA sequence
   codons <- substring(seq_DNA,
-                      first = seq(from = start, to = length-3+1, by = 3), #starting position of codons
-                      last = seq(from = 3+start-1, to = length, by = 3)) #ending position of codons
+                      first = seq(from = start, to = seq_length-3+1, by = 3), #starting position of codons
+                      last = seq(from = 3+start-1, to = seq_length, by = 3)) #ending position of codons
   return(codons) #return of all codons from the sequence
 }
 
--- R/plot_sequence_counts.R
+++ R/plot_sequence_counts.R
@@ -1,30 +1,31 @@
-#' Plots sequence counts
-#'
-#' @param sequence input string of characters to be analyse
-#'
-#' @returns ggplot 2 plot of sequence counts
-#' @export
-#'
-#' @importFrom stringr str_split boundary str_count
-#' @examples
-#' plot_sequence_counts("ATGCGTAC")
-plot_sequence_counts <- function(sequence_string){
-  unique_chars <- sequence_string |>
-    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
-    as.character() |>
-    unique()
+  #' Plots sequence counts
+  #'
+  #' @param sequence input string of characters to be analyse
+  #'
+  #' @returns ggplot 2 plot of sequence counts
+  #' @export
+  #'
+  #' @importFrom stringr str_split boundary str_count
+  #' @examples
+  #' plot_sequence_counts("ATGCGTAC")
+  plot_sequence_counts <- function(sequence_string){
+    unique_chars <- sequence_string |>
+      stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
+      as.character() |>
+      unique()
 
-  counts <- sapply(unique_chars, function(amino_acid) stringr::str_count(string = sequence_string, pattern =  amino_acid)) |>
-    as.data.frame()
+    counts <- sapply(unique_chars, function(amino_acid) stringr::str_count(string = sequence_string, pattern =  amino_acid)) |>
+      as.data.frame()
 
-  colnames(counts) <- c("Counts")
-  counts[["Sequence_string"]] <- rownames(counts)
+    colnames(counts) <- c("Counts")
+    counts$AminoAcid <- rownames(counts)
+    rownames(counts) <- NULL
 
-  sequence_plot <- counts |>
-    ggplot2::ggplot(ggplot2::aes(x = sequence_string, y = Counts, fill = sequence_string)) +
-    ggplot2::geom_col() +
-    ggplot2::theme_bw() +
-    ggplot2::theme(legend.position = "none")
+    sequence_plot <- counts |>
+      ggplot2::ggplot(ggplot2::aes(x = AminoAcid, y = Counts, fill = AminoAcid)) +
+      ggplot2::geom_col() +
+      ggplot2::theme_bw() +
+      ggplot2::theme(legend.position = "none")
 
-  return(sequence_plot)
-}
+    return(sequence_plot)
+  }
